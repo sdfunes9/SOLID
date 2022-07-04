36dfc10b00fbea31d56cd24975e9c28f2c71b77b
Ernesto Zeledon <ernest-zg-te@hotmail.com>
2022-07-04 10:40:00 -0600

    Una prueba desde IntelliJ

--- src/main/java/com/universidad/Main.java
+++ src/main/java/com/universidad/Main.java
@@ -6,36 +6,37 @@ public class Main {
     public static void main(String[] args){
         Scanner x = new Scanner(System.in);
 
-        Menu menu = new Menu();
+        Menu menu = new Menu(new PropiedadesConsola());
         Estudiantes e1 = new Estudiantes();
         Carrera c1 = new Carrera();
         Materia m1 = new Materia();
-        menu.menu();
+
+        menu.imprimirBienvenida();
         int opcion;
         opcion = x.nextInt();
 
-        do {
-            switch(opcion) {
-                case 1: {
-                    e1.registrarEstudiante();
-                    menu.menu();
-                    opcion = x.nextInt();
-                    break;
-                }
-                case 2: {
-                    e1.listarEstudiantes();
-                    menu.menu();
-                    opcion = x.nextInt();
-                    break;
-                }
-                case 3: {
-                    System.exit(3);
-                    break;
-                }
-
-
-            }
-        }while(opcion != 3);
+//        do {
+//            switch(opcion) {
+//                case 1: {
+//                    e1.registrarEstudiante();
+//                    menu.menu();
+//                    opcion = x.nextInt();
+//                    break;
+//                }
+//                case 2: {
+//                    e1.listarEstudiantes();
+//                    menu.menu();
+//                    opcion = x.nextInt();
+//                    break;
+//                }
+//                case 3: {
+//                    System.exit(3);
+//                    break;
+//                }
+//
+//
+//            }
+//        }while(opcion != 3);
 
     }
 }
